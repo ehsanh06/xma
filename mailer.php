34d9b2699e78fd120740f1a72272b44b44d7cf92
Unknown <ehsanhussain991@gmail.com>
2018-06-28 15:20:56 +0100

    changed recipient email

--- mailer.php
+++ mailer.php
@@ -20,7 +20,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "hello@example.com";
+        $recipient = "manuel@gameyfi.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
